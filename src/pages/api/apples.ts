2dc169731446fdc36ee19deba4cfc526b546dc6b
Tom Randus <tom.randus@sapho.com>
2020-01-29 11:00:19 +0100

    Display error

--- src/pages/api/apples.ts
+++ src/pages/api/apples.ts
@@ -4,6 +4,13 @@ import { NextApiRequest, NextApiResponse } from 'next'
 const client = new PrismaClient()
 
 export default async function generate(req: NextApiRequest, res: NextApiResponse) {
-  const apples = await client.apples.findMany({})
-  res.send(`apples ${apples.length}`)
+  try {
+    await client.apples.create({ data: { color: 'green' } })
+    const apples = await client.apples.findMany({})
+    res.send(`apples ${apples.length}`)
+  } catch (e) {
+    res.status(500)
+    console.log(e)
+    res.send(e.message)
+  }
 }
